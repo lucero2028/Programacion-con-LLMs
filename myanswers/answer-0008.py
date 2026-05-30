5344798c10815e8b1da81e841a2c6eb172580685
lucero2028 <lucero.florez@udea.edu.co>
2026-05-30 14:46:39 -0500

    Update answer-0008.py

--- myanswers/answer-0008.py
+++ myanswers/answer-0008.py
@@ -1,1 +1,13 @@
+import numpy as np
+from sklearn.ensemble import RandomForestClassifier
 
+
+def predecir_resistencia(X_train, y_train, X_test):
+
+    modelo = RandomForestClassifier(random_state=42)
+
+    modelo.fit(X_train, y_train)
+
+    predicciones = modelo.predict(X_test)
+
+    return predicciones
