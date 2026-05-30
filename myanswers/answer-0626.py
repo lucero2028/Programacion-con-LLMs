f4eef241ca65c1eb9a3abe8956b2232c06704383
lucero2028 <lucero.florez@udea.edu.co>
2026-05-30 14:48:04 -0500

    Update answer-0626.py

--- myanswers/answer-0626.py
+++ myanswers/answer-0626.py
@@ -1,1 +1,18 @@
+import pandas as pd
 
+def clasificar_base_lubricante(muestras):
+
+    clasificaciones = []
+
+    for _, fila in muestras.iterrows():
+
+        if (
+            fila['viscosity_index'] > 150
+            and
+            fila['flash_point'] > 230
+        ):
+            clasificaciones.append('Sintético')
+        else:
+            clasificaciones.append('Mineral')
+
+    return clasificaciones
