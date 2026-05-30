80b092da01e01eb6d454644ef266cdbecce8395f
lucero2028 <lucero.florez@udea.edu.co>
2026-05-30 14:45:11 -0500

    Update answer-0650.py

--- myanswers/answer-0650.py
+++ myanswers/answer-0650.py
@@ -1,1 +1,33 @@
+import numpy as np
+from sklearn.linear_model import LogisticRegression
+from sklearn.tree import DecisionTreeClassifier
+from sklearn.neighbors import KNeighborsClassifier
+from sklearn.model_selection import cross_val_score
 
+
+def comparar_modelos_clasificacion(X, y):
+
+    modelos = {
+        "lr": LogisticRegression(),
+        "dt": DecisionTreeClassifier(),
+        "knn": KNeighborsClassifier()
+    }
+
+    resultados = {}
+
+    for nombre, modelo in modelos.items():
+        scores = cross_val_score(
+            modelo,
+            X,
+            y,
+            cv=5
+        )
+
+        resultados[nombre] = np.mean(scores)
+
+    mejor = max(
+        resultados,
+        key=resultados.get
+    )
+
+    return (resultados, mejor)
